aa62d1693331609f8dc62663b179e6c25d1c8c42
Pedro Coelho <tp.pedroh.silva@d0007azurecloudtotvscom.onmicrosoft.com>
2022-12-23 12:02:27 -0300

    feat(spaces): add hability to configure in env variable

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,6 @@
+require("dotenv").config();
+
 //This should be the full URL, not just the spaceId\\spaceName
-export const SPACE_URLS = [
+export const SPACE_URLS = process.env.SPACE_URLS?.split(",") || [
   "https://app.gather.town/app/arIfmcBhsz7Hiedn/Testisland-Office"
 ];
